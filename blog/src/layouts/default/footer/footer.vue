55e941539f153a8da3b2a391b5c92ec51a0060f2
backlighting <pitilessmiracle@outlook.com>
2024-11-23 19:13:31 +0800

    feat:备案信息

--- blog/src/layouts/default/footer/footer.vue
+++ blog/src/layouts/default/footer/footer.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="w-full h-[60px] bg-red-300"></div>
+  <div class="w-full h-[60px] pt-[10px] border border-solid border-default-border">
+    <div class="flex justify-center">
+      <span class="text-default-text">
+        © 2024 | 晋ICP备2024050623号
+      </span>
+
+    </div>
+
+  </div>
 </template>
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
