9059019a71638ebf4a92208d7868121645bbe48b
backlighting <pitilessmiracle@outlook.com>
2024-12-17 22:50:08 +0800

    feat:后端域名备案成功，支持https通信，前端修改相关dev

--- blog/src/utils/http/index.ts
+++ blog/src/utils/http/index.ts
@@ -5,7 +5,8 @@ import { CustomTransform } from './customTransform' // 可选的自定义 transf
 const http = new AxiosService(
   {
     baseURL: import.meta.env.VITE_API_BASE_URL || '/api/v0',
-    timeout: 5000
+    timeout: 5000,
+    withCredentials: true, 
   },
   new CustomTransform()
 )
