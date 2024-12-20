15ac3244a1d1f41969f34ef9cbdf207abbde4f84
backlighting <pitilessmiracle@outlook.com>
2024-12-20 22:50:49 +0800

    feat:Avatar组件新增尺寸码

--- blog/src/components/Avatar.vue
+++ blog/src/components/Avatar.vue
@@ -1,40 +1,62 @@
 <template>
-  <div class="relative flex justify-center items-center flex-shrink-0 " :class="{ 'circle': rainbow }">
-    <el-avatar :size="size" :src="props.src" :shape="shape" :fit="fit" @error="errorHandler">
-      <img :src="props.src" v-if="props.src" />
-      <iconUser v-if="!props.src" class="user-default" @click="userLogin" />
+  <div
+    class="relative flex justify-center items-center flex-shrink-0"
+    :class="{ 'circle': rainbow }"
+    :style="computedStyle"
+  >
+    <el-avatar
+      :size="computedSize"
+      :src="src"
+      :shape="shape"
+      :fit="fit"
+      @error="errorHandler"
+    >
+      <img :src="src" v-if="src" />
+      <slot v-else>
+        <iconUser class="user-default" @click="userLogin" />
+      </slot>
     </el-avatar>
   </div>
 </template>
+
 <script setup lang="ts">
 import iconUser from '@/components/icons/iconUser.vue'
+
 const props = defineProps({
-  // 头像相关
   src: { type: String, default: '' },
-  size: { type: Number, default: 40 },
+  size: { type: [Number, String], default: 'middle' }, // number or predefined size
   shape: { type: String, default: 'circle' },
   fit: { type: String, default: 'cover' },
   rainbow: { type: Boolean, default: true }
 })
-const errorHandler = () => true
-const userLogin = () => {
 
+const sizeMap = {
+  small: 32,
+  middle: 45,
+  large: 64
 }
-</script>
-<style lang="scss" scoped>
-.circle {
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  background: conic-gradient(#f00, #ff0, #0f0, #0ff, #00f, #f0f, #f00);
 
-  @media (max-width: 768px) {
-    width: 40px;
-    height: 40px;
+const computedSize = computed(() => {
+  return typeof props.size === 'number' ? props.size : sizeMap[props.size] || sizeMap.middle
+})
+
+const computedStyle = computed(() => {
+  if (!props.rainbow) return {}
+  const haloSize = typeof props.size === 'number' ? props.size + 10 : (sizeMap[props.size] || sizeMap.middle) + 10
+  return {
+    width: `${haloSize}px`,
+    height: `${haloSize}px`,
+    borderRadius: '50%',
+    background: 'conic-gradient(#f00, #ff0, #0f0, #0ff, #00f, #f0f, #f00)'
   }
-}
+})
 
+const errorHandler = () => true
+const userLogin = () => {}
+</script>
+
+<style lang="scss" scoped>
 .user-default {
-  @apply text-default-text
+  @apply text-default-text;
 }
 </style>
