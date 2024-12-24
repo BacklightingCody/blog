a9e930ccf6bb100f00e3a1a988c08b3e6981c72e
backlighting <pitilessmiracle@outlook.com>
2024-12-24 08:23:05 +0800

    fix:删除多余引用的InputField组件

--- blog/src/views/more/index.vue
+++ blog/src/views/more/index.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="bg-dark-100 w-[300px] h-[200px] flex justify-center mt-[100px]">
     <div class="flex flex-col items-center justify-center">
-      <InputField placeholder="请输入" @input-change="handleChange"  @submit="handleSubmit"/>
-    <InputField placeholder="输入" @input-change="handleChange"  @submit="handleSubmit"/>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import { getMoreApi } from '@/services/more'
 // import { refreshToken } from '@/utils/refreshToken'
-import InputField from '@/components/InputField.vue';
 import { useGlobalStore, useUserStore } from '@/stores'
 const userStore = useUserStore()
 const run = () => {
@@ -17,11 +14,5 @@ const run = () => {
     console.log(111)
   })
 }
-const handleChange = (content) => {
-  console.log(content);
-};
-const handleSubmit = () => {
-  console.log('submit');
-};
 </script>
 <style lang="scss" scoped></style>
