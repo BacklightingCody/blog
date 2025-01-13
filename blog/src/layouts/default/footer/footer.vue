1b47d572ca3520a7859fb82557ecc0dbbcbd3378
backlighting <pitilessmiracle@outlook.com>
2025-01-13 22:18:28 +0800

    feat:footer中添加GithubStar组件ui

--- blog/src/layouts/default/footer/footer.vue
+++ blog/src/layouts/default/footer/footer.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="w-full h-[60px] pt-[10px] border border-solid border-default-border">
-    <div class="flex justify-center">
+    <div class="flex justify-center h-full items-center">
       <a class="text-default-text" href="https://beian.miit.gov.cn/#/Integrated/index">
         © 2024 | 	晋ICP备2024050623号-1
       </a>
-
+      <GithubStar class="mx-5" />
     </div>
-
+   
   </div>
 </template>
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
+import GithubStar from '@/components/GithubStar.vue'
 </script>
 <style lang="scss" scoped></style>
